19b3c480dbf7dbc685c7c61449eae472d77bfa6c
JonathanMarandeau <jonathan.marandeau@gmail.com>
2018-04-11 15:41:00 +0200

    Ajout de la fonction hide pour le texte qui sommes nous pour plus de lisibilité

--- script.js
+++ script.js
@@ -2,22 +2,26 @@ $('#navAdidas').click(function() {
   $('.Nike').hide();
   $('.Puma').hide();
   $('.Adidas').show();
+  $('#weAre').hide();
 })
 
 $('#navNike').click(function() {
   $('.Adidas').hide();
   $('.Puma').hide();
   $('.Nike').show();
+  $('#weAre').hide();
 })
 
 $('#navPuma').click(function() {
   $('.Nike').hide();
   $('.Adidas').hide();
   $('.Puma').show();
+  $('#weAre').hide();
 })
 
 $('#navAll').click(function() {
   $('.Nike').show();
   $('.Adidas').show();
   $('.Puma').show();
+  $('#weAre').hide();
 })
