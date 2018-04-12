84f29c0fd82249e294b4145812637c523228259f
JonathanMarandeau <jonathan.marandeau@gmail.com>
2018-04-12 09:56:48 +0200

    modif script

--- script.js
+++ script.js
@@ -1,27 +1,29 @@
-$('#navAdidas').click(function() {
-  $('.Nike').hide();
-  $('.Puma').hide();
-  $('.Adidas').show();
-  $('#weAre').hide();
-})
+$(function(){
+  $('#navAdidas').click(function() {
+    $('.Nike').hide();
+    $('.Puma').hide();
+    $('.Adidas').show();
+    $('#weAre').hide();
+  })
 
-$('#navNike').click(function() {
-  $('.Adidas').hide();
-  $('.Puma').hide();
-  $('.Nike').show();
-  $('#weAre').hide();
-})
+  $('#navNike').click(function() {
+    $('.Adidas').hide();
+    $('.Puma').hide();
+    $('.Nike').show();
+    $('#weAre').hide();
+  })
 
-$('#navPuma').click(function() {
-  $('.Nike').hide();
-  $('.Adidas').hide();
-  $('.Puma').show();
-  $('#weAre').hide();
-})
+  $('#navPuma').click(function() {
+    $('.Nike').hide();
+    $('.Adidas').hide();
+    $('.Puma').show();
+    $('#weAre').hide();
+  })
 
-$('#navAll').click(function(){
-  $('.Nike').show();
-  $('.Adidas').show();
-  $('.Puma').show();
-  $('#weAre').show();
-})
+  $('#navAll').click(function(){
+    $('.Nike').show();
+    $('.Adidas').show();
+    $('.Puma').show();
+    $('#weAre').show();
+  })
+});
